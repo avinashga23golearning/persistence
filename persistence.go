15071689b6c12514d8da62b90875f1fadb3fb8f1
Avinash G A <avinashga23@gmail.com>
2018-03-19 21:06:45 +0530

    added person persistence manager type

---
+++ persistence.go
@@ -0,0 +1,70 @@
+package persistence
+
+import (
+	driver "github.com/arangodb/go-driver"
+	"github.com/arangodb/go-driver/http"
+	"github.com/avinashga23golearning/model"
+)
+
+var client driver.Client
+
+func init() {
+	client, _ = newDriver()
+}
+
+//PersonPersistenceManager type
+type PersonPersistenceManager struct{}
+
+//NewPersonPersistenceManager new person persistence manager
+func NewPersonPersistenceManager() *PersonPersistenceManager {
+	personPersistenceManager := PersonPersistenceManager{}
+
+	return &personPersistenceManager
+}
+
+func newDriver() (driver.Client, error) {
+	conn, _ := http.NewConnection(http.ConnectionConfig{
+		Endpoints: []string{"http://192.168.100.10:8259"},
+	})
+
+	return driver.NewClient(driver.ClientConfig{
+		Connection: conn,
+	})
+}
+
+//CreatePerson creates person
+func (PersonPersistenceManager) CreatePerson(person model.Person) string {
+	person.ID = ""
+
+	db, _ := client.Database(nil, "_system")
+	col, _ := db.Collection(nil, "person")
+
+	meta, _ := col.CreateDocument(nil, person)
+	return meta.Key
+}
+
+//DeletePerson deletes person
+func (PersonPersistenceManager) DeletePerson(id string) {
+	db, _ := client.Database(nil, "_system")
+	col, _ := db.Collection(nil, "person")
+
+	col.RemoveDocument(nil, id)
+}
+
+//UpdatePerson updates person
+func (PersonPersistenceManager) UpdatePerson(person model.Person) {
+	db, _ := client.Database(nil, "_system")
+	col, _ := db.Collection(nil, "person")
+
+	col.UpdateDocument(nil, person.ID, person)
+}
+
+//GetPersonByID gets person by id
+func (PersonPersistenceManager) GetPersonByID(id string) model.Person {
+	db, _ := client.Database(nil, "_system")
+	col, _ := db.Collection(nil, "person")
+	var person model.Person
+
+	col.ReadDocument(nil, id, &person)
+	return person
+}
